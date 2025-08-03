0a1c7fa3e93582efddfe6f4d031379e64d032ef1
Nafiz <nafizfuadsharkar@gmail.com>
2025-08-04 02:03:21 +0600

    solution added

--- allSolve.js
+++ allSolve.js
@@ -41,7 +41,7 @@ if (
 }
 
 /** Problem 05 - ( PH Email Generator )  */
-var student = { name: "jhankar", roll: 1014, department: "cse" };
+var student = { name: "kuddus", roll: 1014, department: "cse" };
 //write your code here
 
 var email =
--- problem5.js
+++ problem5.js
@@ -1,5 +1,5 @@
 /** Problem 05 - ( PH Email Generator )  */
-var student = { name: "jhankar", roll: 1014, department: "cse" };
+var student = { name: "kuddus", roll: 1014, department: "cse" };
 //write your code here
 
 var email =
